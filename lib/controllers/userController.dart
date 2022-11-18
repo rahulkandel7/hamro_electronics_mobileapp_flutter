9d360a4cc2c330c8040c44688d1cacdd9452abc2
Rahul Kandel <rahulkandel7@gmail.com>
2022-11-18 11:06:41 +0545

    reamining in edit profile

--- lib/controllers/userController.dart
+++ lib/controllers/userController.dart
@@ -68,6 +68,78 @@ class UserController extends StateNotifier<List<User>> {
 
     return response;
   }
+
+  //* Function to change password
+  Future<http.Response> changePassword(
+    String currentPassword,
+    String newPassword,
+  ) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final response =
+        await http.post(Uri.parse('${url}user/changepass'), headers: {
+      'Authorization': 'Bearer ${prefs.getString('token')}',
+    }, body: {
+      'current_password': currentPassword,
+      'new_password': newPassword,
+    });
+
+    return response;
+  }
+
+  //* Function to update Info
+  Future<http.Response> updateInfo(
+    String name,
+    String phone,
+    String address,
+    String photopath,
+  ) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (photopath == '') {
+      final response =
+          await http.post(Uri.parse('${url}user/update'), headers: {
+        'Authorization': 'Bearer ${prefs.getString('token')}',
+      }, body: {
+        'name': name,
+        'address': address,
+        'phone_number': phone,
+      });
+
+      if (response.statusCode == 200) {
+        prefs.remove('user');
+        final res = json.decode(response.body);
+        prefs.setString('user', json.encode(res['data']));
+      }
+      return response;
+    } else {
+      final jsons = {
+        'name': name,
+        'address': address,
+        'phone_number': phone,
+      };
+      Map<String, String> headers = {
+        'Content-Type': "multipart/form-data",
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ${prefs.getString('token')}',
+      };
+
+      var request = http.MultipartRequest(
+          'POST', Uri.parse("${url}user/update"))
+        ..fields.addAll(jsons)
+        ..headers.addAll(headers)
+        ..files
+            .add(await http.MultipartFile.fromPath('profile_photo', photopath));
+
+      var streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+      if (response.statusCode == 200) {
+        prefs.remove('user');
+        final res = json.decode(response.body);
+        prefs.setString('user', json.encode(res['data']));
+      }
+      print(response.body);
+      return response;
+    }
+  }
 }
 
 StateNotifierProvider<UserController, List<User>> userProvider =
